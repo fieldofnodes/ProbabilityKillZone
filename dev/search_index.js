a281dc43e30224b2929fe336a1a021738a57b423
Documenter.jl <documenter@juliadocs.github.io>
2024-08-09 15:58:01 +0000

    build based on 55c7262

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProbabilityKillZone","category":"page"},{"location":"#ProbabilityKillZone","page":"Home","title":"ProbabilityKillZone","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProbabilityKillZone.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProbabilityKillZone]","category":"page"}]
+}
